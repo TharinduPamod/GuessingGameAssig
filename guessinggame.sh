960e9f44b3ae681c70ea88406451d3f1e3db1fde
tharindu <tharindupamod@yahoo.com>
2020-04-24 03:04:41 -0700

    add guessinggame

---
+++ guessinggame.sh
@@ -0,0 +1,19 @@
+function direct {
+
+	count=2
+	while [[ $count -gt 1 ]]
+	do
+	dir=$(ls |wc -l)
+ 		echo "enter your guess of num of files:"
+        	read guess
+		if [[ $guess -eq $dir ]]
+		then
+			echo "correct"
+			break
+		else
+			echo "incorrect"
+		fi	
+	let count=$count+1
+	done
+	}
+direct
